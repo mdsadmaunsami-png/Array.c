3c004075fe618a0f35bb2255610d0f36bb56aabf
MD SAD MAUN SAMI <mdsadmaunsami@gmail.com>
2026-03-04 00:42:40 +0600

    array size input and summation output

--- sum_array.c
+++ sum_array.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,sum=0;
+    int a,i,sum=0;
     printf("Enter the size of array: ");
-    scanf("%d",&n);
-    int num[n];
-    for(i=0;i<n;i++)
+    scanf("%d",&a);
+    int num[a];
+    for(i=0;i<a;i++)
     {
         printf("Enter value %d: ",i+1);
         scanf("%d",&num[i]);
         sum=sum+num[i];
     }
-    printf("Summation of all %d values : %d\n",n,sum);
+    printf("Summation of all %d values : %d\n",a,sum);
     return 0;
 }
