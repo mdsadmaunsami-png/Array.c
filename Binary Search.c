112359deb80a8ab1935dd26aff1de6a753a1940b
MD SAD MAUN SAMI <mdsadmaunsami@gmail.com>
2026-05-21 00:53:20 +0600

    Correct binary search implementation in C
    Fixed syntax issues and improved binary search logic.

---
+++ Binary Search.c
@@ -0,0 +1,69 @@
+#include<stdio.h>
+int main()
+{
+    int LA[100],n,target,count=0;
+    printf("Enter n and Target: ");
+    scanf("%d %d",&n,&target);
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&LA[i]);
+    }
+    int low = 0,high = n-1;
+    while(low<=high)
+    {
+        int mid= (low+high)/2;
+        if(LA[mid]==target)
+        {
+            printf("Found: %d",LA[mid]);
+            count++;
+            return 0;
+        }
+        else if(LA[mid]<target)
+        {
+            low= mid+1;
+        }
+        else
+        {
+            high= mid-1;
+        }
+    }
+    if(count==0)
+    {
+        printf("Not found");
+    }
+    return 0;
+}
+
+---------- without count ------------
+
+#include<stdio.h>
+int main()
+{
+    int LA[100],n,target;
+    printf("Enter n and Target: ");
+    scanf("%d %d",&n,&target);
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&LA[i]);
+    }
+    int low = 0,high = n-1;
+    while(low<=high)
+    {
+        int mid= (low+high)/2;
+        if(LA[mid]==target)
+        {
+            printf("Found: %d",LA[mid]);
+            return 0;
+        }
+        else if(LA[mid]<target)
+        {
+            low= mid+1;
+        }
+        else
+        {
+            high= mid-1;
+        }
+    }
+    printf("Not found");
+    return 0;
+}
